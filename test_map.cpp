8027f76115351bb252bbfe9cd0075ff39f52947e
dwanett <Niki503@yandex.ru>
2022-01-22 19:40:35 +0300

    fix

--- test_map.cpp
+++ test_map.cpp
@@ -5,7 +5,6 @@
 #include <map>
 #include "map.hpp"
 
-/*
 int main ()
 {
 	std::map<int,int> orig_map;
@@ -198,84 +197,4 @@ int main ()
 	my_map.clear();
 	std::cout << "my_map: " << "size = " << my_map.size() << std::endl;
 	return 0;
-}*/
-
-#define T1 int
-#define T2 std::string
-
-ft::map<T1, T2> mp;
-ft::map<T1, T2>::iterator it = mp.end();
-
-template <typename T>
-std::string	printPair(const T &iterator, bool nl = true, std::ostream &o = std::cout)
-{
-	o << "key: " << iterator->first << " | value: " << iterator->second;
-	if (nl)
-		o << std::endl;
-	return ("");
-}
-
-template <typename T_MAP>
-void	printSize(T_MAP const &mp, bool print_content = 1)
-{
-	std::cout << "size: " << mp.size() << std::endl;
-	std::cout << "max_size: " << mp.max_size() << std::endl;
-	if (print_content)
-	{
-		typename T_MAP::const_iterator it = mp.begin(), ite = mp.end();
-		std::cout << std::endl << "Content is:" << std::endl;
-		for (; it != ite; ++it)
-			std::cout << "- " << printPair(it, false) << std::endl;
-	}
-	std::cout << "###############################################" << std::endl;
-}
-
-void	ft_find(T1 const &k)
-{
-	ft::map<T1, T2>::iterator ret = mp.find(k);
-
-	if (ret != it)
-		printPair(ret);
-	else
-		std::cout << "map::find(" << k << ") returned end()" << std::endl;
-}
-
-void	ft_count(T1 const &k)
-{
-	std::cout << "map::count(" << k << ")\treturned [" << mp.count(k) << "]" << std::endl;
-}
-
-int		main(void)
-{
-	mp[42] = "fgzgxfn";
-	mp[25] = "funny";
-	mp[80] = "hey";
-	mp[12] = "no";
-	mp[27] = "bee";
-	mp[90] = "8";
-	printSize(mp);
-
-	std::cout << "\t-- FIND --" << std::endl;
-	ft_find(12);
-	ft_find(3);
-	ft_find(35);
-	ft_find(90);
-	ft_find(100);
-
-	std::cout << "\t-- COUNT --" << std::endl;
-	ft_count(-3);
-	ft_count(12);
-	ft_count(3);
-	ft_count(35);
-	ft_count(90);
-	ft_count(100);
-
-	mp.find(27)->second = "newly inserted mapped_value";
-
-	printSize(mp);
-
-	ft::map<T1, T2> const c_map(mp.begin(), mp.end());
-	std::cout << "const map.find(" << 42 << ")->second: [" << c_map.find(42)->second << "]" << std::endl;
-	std::cout << "const map.count(" << 80 << "): [" << c_map.count(80) << "]" << std::endl;
-	return (0);
 }
